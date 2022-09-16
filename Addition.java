d68a2f3f774f2d9cbf8d2e31aaac0be8c69198e2
Pranavanath Margala <mpvnath00@gmail.com>
2022-09-16 09:59:42 +0530

    updated Addition of two numbers java Program

--- Addition.java
+++ Addition.java
@@ -3,6 +3,6 @@ class Addition
  public static void main(String args[])
   {
     int num1=20,num2=30;
-    System.out.println(num1+num2);
+    System.out.println("Addition of two numbers is : "+(num1+num2));
   }
 }
